284f817373de1c17ab37e68364ff1662f57ea382
junjun526 <zm26179@josai.ac.jp>
2026-07-08 21:57:08 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,9 @@
 alert('Hello, junpei!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+2の平方根ち３の平方根の和
+１を０で割るとどうなるか、試してみなさい。
+０を０で割るとどうなるか、試してみなさい。
 alert(2+3);
 alert('2'+'3');
 alert(Number('2')+Number('3'));
